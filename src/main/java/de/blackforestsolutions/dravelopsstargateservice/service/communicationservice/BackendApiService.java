3f4f1f8e01d6ef5a9bd204c5d1a64a50a9afaf13
hennihaus <jan-hendrik.hausner@outlook.com>
2020-10-28 16:37:50 +0100

    made build runnable #minor

--- src/main/java/de/blackforestsolutions/dravelopsstargateservice/service/communicationservice/BackendApiService.java
+++ src/main/java/de/blackforestsolutions/dravelopsstargateservice/service/communicationservice/BackendApiService.java
@@ -1,7 +1,6 @@
 package de.blackforestsolutions.dravelopsstargateservice.service.communicationservice;
 
 import de.blackforestsolutions.dravelopsdatamodel.CallStatus;
-import de.blackforestsolutions.dravelopsdatamodel.Journey;
 import de.blackforestsolutions.dravelopsdatamodel.util.ApiToken;
 import reactor.core.publisher.Flux;
 
