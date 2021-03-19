a82f5dacb755ceff6ae2c2c18e45195dfff87b8c
hennihaus <jan-hendrik.hausner@outlook.com>
2021-03-19 20:27:45 +0100

    made build runnable #patch

--- src/main/java/de/blackforestsolutions/dravelopsstargateservice/configuration/GeocodingConfiguration.java
+++ src/main/java/de/blackforestsolutions/dravelopsstargateservice/configuration/GeocodingConfiguration.java
@@ -7,4 +7,5 @@ public class GeocodingConfiguration {
     public static final double MAX_WGS_84_LONGITUDE = 180.0d;
     public static final double MIN_WGS_84_LATITUDE = -90.0d;
     public static final double MAX_WGS_84_LATITUDE = 90.0d;
+
 }
